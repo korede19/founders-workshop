669c61f70d27889b63d889c98388891d090f8d4a
Oyeyemi <mac@Oyeyemi-Korede.local>
2025-11-23 13:33:45 +0100

    update

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -5,6 +5,7 @@ import Twitter from "@/svgs/twittter";
 import Linkedln from "@/svgs/linkedln";
 import Facebook from "@/svgs/facebook";
 import Instagram from "@/svgs/instagram";
+import Link from "next/link";
 
 const Footer = () => {
   return (
@@ -12,9 +13,9 @@ const Footer = () => {
       <div className={styles.footerContent}>
         <Image src="/assets/logo-new.png" alt="Logo" width={150} height={50} />
         <div className={styles.links}>
-          <p>How it Works</p>
-          <p>Features</p>
-          <p>Contact Us</p>
+          <Link href="#how-it-works">How it Works</Link>
+          <Link href="#features">Features</Link>
+          <Link href="#contact">Contact Us</Link>
         </div>
         <div className={styles.socials}>
           <Twitter />
